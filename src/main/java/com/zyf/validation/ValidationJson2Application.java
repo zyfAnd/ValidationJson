294d52cbdbf253f1553890646e651c6136f369cd
zyfand <1105564280@qq.com>
2017-08-07 16:10:46 +0800

    validation-2

---
+++ src/main/java/com/zyf/validation/ValidationJson2Application.java
@@ -0,0 +1,12 @@
+package com.zyf.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationJson2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationJson2Application.class, args);
+	}
+}
